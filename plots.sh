4088cddf50d9066b9308417f6307ba583fc0ead7
Dr. Adam Streck <adam.streck@gmail.com>
2022-05-16 11:20:51 +0200

    Added conda, updated readme.

--- plots.sh
+++ plots.sh
@@ -8,7 +8,7 @@ smooth=2
 pyfish $out/populations.csv $out/parent_tree.csv $out/fish.png -R $col -S $smooth
 pyfish $out/populations.csv $out/parent_tree.csv $out/fish_abs.png -R $col -S $smooth -a  
 echo "Plotting Parent Graph"
-dot -Tpng $out/parent_graph.dot > $out/parent_tree.png
+dot -Tpng $out/parent_graph.dot > $out/parent_graph.png
 #echo "Plotting Population Dynamics"
 #python3 scripts/plot_population_dynamics.py --input_folder $out --output_folder $out
 echo "plotting Metrics Overview"
