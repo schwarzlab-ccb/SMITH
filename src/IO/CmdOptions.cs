2720df35037d3210d5b7dcf694fc23ef2581b814
Adam Streck <adam.streck@gmail.com>
2026-04-20 14:49:37 +0200

    Fixed warnings

--- src/IO/CmdOptions.cs
+++ src/IO/CmdOptions.cs
@@ -6,11 +6,11 @@ public class CmdOptions
 {
     [Option('O', "output", Required = false, Default = "./out", 
         HelpText = "The path to the output files.")]
-    public string OutputPath { get; set; }
+    public string OutputPath { get; set; } = "./out";
 
     [Option('C', "config", Required = false, Default = "", 
         HelpText = "A json file with configuration of the experiment.")]
-    public string ConfigFile { get; set; }
+    public string ConfigFile { get; set; } = "";
 
     [Option('N', Required = false, Default = false,
         HelpText = "Use newline in logs (useful for batch execution)")]
